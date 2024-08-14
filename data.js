682ea345d2893cfa9ae865eb3d72b46190bac061
OksanaBer <berdovskaoksana@gmail.com>
2024-08-14 11:35:08 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "Вид 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09398362062039567,
+          "pitch": 0.19965792198721743,
+          "rotation": 0,
+          "target": "1--2"
+        },
+        {
+          "yaw": 1.5385476870881476,
+          "pitch": 0.1897344767523883,
+          "rotation": 0,
+          "target": "2--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2",
+      "name": "Вид 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5825588546366385,
+          "pitch": 0.19999481760677895,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--3",
+      "name": "Вид 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5282264403962387,
+          "pitch": 0.19894747779549427,
+          "rotation": 0,
+          "target": "0--1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
